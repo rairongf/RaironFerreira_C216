0299564bf24d728087577b2499c510f800c586e9
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-09-24 08:31:08 -0300

    feat: add hello controller

---
+++ lab_rest_server/src/main/java/br/inatel/labs/labrest/server/controller/MyMessage.java
@@ -0,0 +1,13 @@
+package br.inatel.labs.labrest.server.controller;
+
+public class MyMessage {
+	private String info;
+	
+	public String getInfo() {
+		return this.info;
+	}
+	
+	public void setInfo(String info) {
+		this.info = info;
+	}
+}
